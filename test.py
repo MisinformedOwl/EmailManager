0019b4a11591dca784e32eaf22c77ca571726755
MisinformedOwl <jaydean1311@gmail.com>
2025-10-04 22:55:09 +0100

    Fully updated

--- test.py
+++
@@ -1,8 +0,0 @@
-import imaplib
-
-try:
-    mail = imaplib.IMAP4_SSL('imap.gmail.com')
-    mail.login('jaydean1311@gmail.com', 'vqvu ibvs qoip qgii')
-    print("✅ Login successful")
-except imaplib.IMAP4.error as e:
-    print("❌ Login failed:", e)
